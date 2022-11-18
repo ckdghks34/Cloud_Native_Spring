a60b2200b3bf76fbdeab5616a0a258d52efacb39
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-11-18 20:54:02 +0900

    APIgateway-Loadbalancer

--- CloudNative_Spring/Spring_Gateway/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ CloudNative_Spring/Spring_Gateway/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,15 +1,24 @@
 package com.example.secondservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/second-service")
 @Slf4j
 public class SecondServiceController {
+    Environment env;
+
+    public SecondServiceController(Environment env){
+        this.env = env;
+    }
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second service.";
@@ -22,7 +31,8 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there, This is a message from Second Services";
+    public String check(HttpServletRequest request){
+        log.info("Server port= {}",request.getServerPort());
+        return String.format("Hi, there, This is a message from Second Services on Port %s", env.getProperty("local.server.port"));
     }
 }
